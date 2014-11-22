5dba83ec10a187ab2ebf5a78d7fef218bc7a124a
LaoLin <hi@laolin.com>
2014-11-23 01:30:16 +0800

    _list界面下，把自动翻页 也删了

--- data/_list/Reveal.config.js
+++ data/_list/Reveal.config.js
@@ -2,6 +2,7 @@
 //Reveal.data.mdPath
 var ep=document.getElementById('slide-container')
 ep.removeChild(ep.lastChild)
+ep.children[0].removeChild(ep.children[0].getElementsByClassName('autoPlay')[0])
 Reveal.configure({
 
 })
